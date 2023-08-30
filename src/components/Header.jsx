babbb42f3bcac8293ef46d93dee8a7312414e6a0
Alvaro Sanchez <alvaro_sanchez13@hotmail.com>
2023-08-30 15:30:27 -0300

    cambio url banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,18 @@ export default function Header() {
     <>
         <nav className="navbar navbar-expand-sm  fixed-top">
         
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#">Navbar</a>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                <a className="navbar-brand" href="#">Navbar</a>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Enlace</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Enlace</a>
                     </li>
                     
 
@@ -38,7 +38,7 @@ export default function Header() {
                 </div>
             </nav>
         <div className='primero' style={{justifyContent:'center',alignContent:'center',backgroundRepeat: 'no-repeat', backgroundPosition: 'center center' }}>
-            <img src="src/img/banner.png" alt="" className='d-block w-100'/>
+            <img src="https://github.com/Alvaro-Antonii/img/blob/main/banner.png?raw=true" alt="" className='d-block w-100'/>
             <div className='cont-bot'>
                 <Boton cont={"Asia"}/>
                 <Boton cont={"Europa"}/>
